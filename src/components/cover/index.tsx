9b698bfc8ae1597eee0fb40ec30b468217219e00
xd-Abi <abinayansureskumar@gmail.com>
2023-04-07 19:09:58 +0200

    Fixed eslint errors

--- src/components/cover/index.tsx
+++ src/components/cover/index.tsx
@@ -3,7 +3,7 @@ export default function Cover() {
     <div className="min-h-screen flex justify-center items-center">
       <div>
         <p className="text-center font-bold text-dim text-cover-caption">
-          Welcome, I'm
+          Welcome, I am
         </p>
         <p className="text-center font-bold text-vivid text-cover-title">
           xd Abi.
